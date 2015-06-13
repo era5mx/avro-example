096cc00d61c61770e4744b9f92027b6ea7872eb7
David Rengifo <david@rengifo.mx>
2015-06-13 01:02:42 -0500

    se restablece la version perdida de avro

--- src/main/java/mx/com/extend/example/Avro.java
+++ src/main/java/mx/com/extend/example/Avro.java
@@ -3,10 +3,15 @@
  */
 package mx.com.extend.example;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
@@ -14,6 +19,9 @@ import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.commons.lang.StringUtils;
+
+import com.thoughtworks.xstream.XStream;
 
 import avro.gen.User;
 
@@ -23,11 +31,18 @@ import avro.gen.User;
  */
 public class Avro {
 
-	private static final String NAME_FILE_AVRO = "nameFileAvro";
+	/** Constante para cargar el path configurado */
 	private static final String PATH_AVRO = "pathAvro";
+	/** Constante para cargar el namefile configurado */
+	private static final String NAME_FILE_AVRO = "nameFileAvro";
+	/** Constante para cargar el path configurado para los XMLs */
+	private static final String RESULT_XML = "pathResultXML";
+	/** Multiplicador */
+	//private static int multiplicador = 1000000;
+	private static int multiplicador = 10;
 
 	/**
-	 *
+	 * Constructor
 	 */
 	public Avro() {
 	}
@@ -40,14 +55,76 @@ public class Avro {
 		List<User> listUsers = avro.generateUsers();
 
 		//Engordamos la lista a 3 millones de registros
-		int i = 1000000;
 		do {
 			listUsers.addAll(avro.generateUsers());
-			--i;
-		} while (i>0);
+			--multiplicador;
+		} while (multiplicador>0);
 
+		//Test generacion del nombre del archivo
+		//System.out.println(UUID.randomUUID().toString().concat(".xml"));
+
+		//Serializamos los usuarios
 		avro.serializing(listUsers);
-		avro.deserializing();
+
+		//Deserializamos los usuarios
+		List<User> listUsersDeserializated = avro.deserializing();
+
+
+		for (User user2 : listUsersDeserializated) {
+
+			//Mostramos los usuarios en consola
+			System.out.println(user2);
+
+			//Genera el XML del User
+			avro.generaXML(user2);
+
+		}
+
+	}
+
+	/**
+	 * @param user
+	 */
+	private void generaXML(User user) {
+
+		XStream xs = new XStream();
+
+		// OBJECT --> XML
+		String xml = xs.toXML(user);
+
+
+
+		// Crea los archivo xml
+		File file = this.createFile(generatePathResult(),
+						UUID.randomUUID().toString().concat(".xml"));
+
+		// Escritura
+		try {
+			FileWriter w = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(w);
+			PrintWriter wr = new PrintWriter(bw);
+			wr.write(xml);// escribimos en el archivo
+			wr.close();
+			bw.close();
+		} catch (IOException e) {
+		}
+
+	}
+
+	/**
+	 * @return
+	 */
+	private String generatePathResult() {
+		String pathResultXML = UtilProperties.getProperty(RESULT_XML)
+				.concat(String.valueOf(Calendar.YEAR))
+				.concat(String.valueOf(Calendar.MONTH))
+				.concat(String.valueOf(Calendar.DAY_OF_MONTH)
+				.concat(String.valueOf(Calendar.HOUR_OF_DAY)
+				.concat(String.valueOf(Calendar.MINUTE)
+				.concat(String.valueOf(Calendar.MILLISECOND)))));
+
+		System.out.println(pathResultXML);
+		return pathResultXML;
 	}
 
 	/**
@@ -109,33 +186,59 @@ public class Avro {
 	}
 
 	/**
+	 * Crea el File con el nombre recibido
+	 * @return File
+	 */
+	private File createFile(String path, String nameFile) {
+		String msgError="";
+		if(StringUtils.isBlank(path)){
+			msgError.concat(" ").concat("[El path recibido es blanco o nulo]");
+		}
+		if(StringUtils.isBlank(nameFile)){
+			msgError.concat(" ").concat("[El nameFile recibido es blanco o nulo]");
+		}
+
+		File file = null;
+
+		if(StringUtils.isBlank(msgError)) {
+			File folder = new File(path);
+			if (!folder.exists()) {
+				folder.mkdirs(); // esto crea la carpeta java, independientemente que exista el path completo, si no existe crea toda la ruta necesaria
+			}
+			String absolutePathFile = folder.getPath().concat("\\").concat(nameFile);
+			System.out.println(absolutePathFile);
+
+			file = new File(absolutePathFile);
+		}
+
+		return file;
+	}
+
+	/**
 	 * Crea el File
 	 * @return File
 	 */
 	private File createFile() {
-		File folder = new File(UtilProperties.getProperty(PATH_AVRO));
-		if (!folder.exists()) {
-			folder.mkdirs(); // esto crea la carpeta java, independientemente que exista el path completo, si no existe crea toda la ruta necesaria
-		}
-		String absolutePathFile = folder.getPath().concat("\\").concat(UtilProperties.getProperty(NAME_FILE_AVRO));
-		System.out.println(absolutePathFile);
-
-		File file = new File(absolutePathFile);
-		return file;
+		return createFile(UtilProperties.getProperty(PATH_AVRO), UtilProperties.getProperty(NAME_FILE_AVRO));
 	}
 
 	/**
 	 * Deserialize Users from disk
 	 */
-	private void deserializing() {
+	private List<User> deserializing() {
 		long tiempoInicio = System.currentTimeMillis();
 		// We create a SpecificDatumReader, analogous to the SpecificDatumWriter we used in serialization, which converts in-memory serialized items into instances of our generated class, in this case User.
 		DatumReader<User> userDatumReader = new SpecificDatumReader<User>(User.class);
+
+		//Creamos la lista de retorno
+		List<User> listUser = new ArrayList<User>();
+
 		// We pass the DatumReader and the previously created File to a DataFileReader, analogous to the DataFileWriter, which reads the data file on disk.
 		try {
 			File file = createFile();
 			@SuppressWarnings("resource")
 			DataFileReader<User> dataFileReader = new DataFileReader<User>(file, userDatumReader);
+
 			User user = null;
 			// Next we use the DataFileReader to iterate through the serialized Users and print the deserialized object to stdout.
 			while (dataFileReader.hasNext()) {
@@ -143,7 +246,8 @@ public class Avro {
 				// allocating and garbage collecting many objects for files with
 				// many items.
 				user = dataFileReader.next(user);
-				System.out.println(user);
+				//agregamos el objeto user a la lista de retorno
+				listUser.add(user);
 			}
 			// DataFileReader no cuenta con un metodo close()
 			dataFileReader=null;
@@ -151,8 +255,15 @@ public class Avro {
 			e.printStackTrace();
 		}
 		terminaProceso("deserializing", tiempoInicio);
+
+		//retornamos la lista de objetos user
+		return listUser;
 	}
 
+	/**
+	 * @param nameProcess
+	 * @param tiempoInicio
+	 */
 	private void terminaProceso(String nameProcess, long tiempoInicio) {
 		long totalTiempo = System.currentTimeMillis() - tiempoInicio;
 		System.out.println("El tiempo del proceso " + nameProcess + " es :" + totalTiempo + " miliseg");
